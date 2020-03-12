3c4a295d902ad278439a177816f20561b3f4cab0
broana-ait <torra.agustinignacio@gmail.com>
2020-03-12 00:36:03 -0300

    added flexbox and create Currencies.js file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import React, { useState } from 'react';
-//import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import { PizzaCard } from './components/PizzaCard';
-import { Confirmation } from './components/Confirmation';
-import pizzas from './data';
-//import CurrencyTable from './components/currencyTable'
+import Currencies from './components/Currencies'
 
-/*import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';*/
-
-/*<CurrencyTable currency='USD' />
-<CurrencyTable currency='AUD' />
-<CurrencyTable currency='JPY' />
-<CurrencyTable currency='RUB' />*/
 
 function App() {
 
-  const[ordered, setOrdered] = useState(false);
-
-  function displayConfirmation() {
-    setOrdered(true);
-
-    setTimeout(() => {
-      setOrdered(false)
-    }, 3000);
-  }
-
   return (
-      <Container>
-        {ordered && <Confirmation toggle={setOrdered} />}
-        <Row>
-          {pizzas.map(data => (
-            <Col xs={3} className="mb-5" key={`${data.id}`}>
-              <PizzaCard data={data} setOrdered={displayConfirmation}></PizzaCard>
-            </Col>
-          ))}
-        </Row>
-      </Container>  
+    <div>
+      <h1 className="mt-5 mb-0">Bitcoin prices</h1>
+      <h5 className="mt-0">Select one currency</h5>
+      <Currencies />
+    </div>
     )
 }
 
---
+++ src/models/Currency.js
@@ -0,0 +1,8 @@
+export class Currency {
+    constructor(last, buy, sell, symbol) {
+      this.last = last;
+      this.buy = buy;
+      this.sell = sell;
+      this.symbol = symbol;
+    }
+  }
