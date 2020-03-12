932048b6d4c37b2e0e72b85dd858799dc65c5917
broana-ait <torra.agustinignacio@gmail.com>
2020-03-12 13:24:46 -0300

    Updates

---
+++ precache-manifest.ed17a55f7db9d1f99eddbc290f57f3af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e42ec284246a46484c8cc84fe68c55e",
+    "url": "/consume-api-with-react/index.html"
+  },
+  {
+    "revision": "b286f3b31277885fb0a8",
+    "url": "/consume-api-with-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e0bb640357801cfee836",
+    "url": "/consume-api-with-react/static/css/main.6d973d26.chunk.css"
+  },
+  {
+    "revision": "b286f3b31277885fb0a8",
+    "url": "/consume-api-with-react/static/js/2.0cd92105.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/consume-api-with-react/static/js/2.0cd92105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bb640357801cfee836",
+    "url": "/consume-api-with-react/static/js/main.d07290de.chunk.js"
+  },
+  {
+    "revision": "74304a7847b2db66a949",
+    "url": "/consume-api-with-react/static/js/runtime-main.910946a7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/consume-api-with-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
